df90f82630a51a6679a51c1bd50177b6aa53245c
EzeaniChucks <concord_chucks2@yahoo.com>
2024-08-11 18:38:08 +0100

    initial commit

---
+++ src/app/components/home/infoSection3.tsx
@@ -0,0 +1,23 @@
+const InfoSection3 = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-5 w-full max-w-screen md:p-10 p-3 mt-[100px]">
+      <div className="flex flex-col items-center justify-center text-gray-100 gap-5">
+        <h3 className="text-4xl text-center">Why should you invest?</h3>
+        <h3 className="w-[50px] h-1 bg-green-600 text-center rounded-xl"></h3>
+      </div>
+      <div className="flex flex-col items-center justify-center text-gray-200 gap-5 text-sm md:text-md w-full bg-white/10 p-5 rounded-xl">
+        <h4 className="text-center">
+          Ever sat down to calculate how much you spend on sharwama and icecream
+        </h4>
+        <h4 className="text-center">Well the amount can quickly add up!</h4>
+        <h4 className="text-center">
+          {
+            "At extraMonies, you not only want to help you save, we want to see you grow. By investing with us, you are helping businesses in your community and while you earn ROI. It's a win-win for every one"
+          }
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default InfoSection3;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import WelcomeNote from "./components/home/WelcomeNote";
 import InfoSection1 from "./components/home/InfoSection1";
 import InfoSection2 from "./components/home/infoSection2";
 import GetAppBTN from "./components/home/getAppBtn";
+import InfoSection3 from "./components/home/infoSection3";
 
 export default function Home() {
   return (
@@ -12,6 +13,8 @@ export default function Home() {
       <InfoSection1 />
       <GetAppBTN />
       <InfoSection2 />
+      <GetAppBTN />
+      <InfoSection3 />
       <GetAppBTN />
     </main>
   );
